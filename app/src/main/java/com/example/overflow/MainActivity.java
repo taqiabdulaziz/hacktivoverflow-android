8f1716bf5f3ca84464b4f74811513622cf0a12fe
taqiabdulaziz <svx.annexiv@gmail.com>
2019-02-01 14:34:04 +0700

    success parse data from server

--- app/src/main/java/com/example/overflow/MainActivity.java
+++ app/src/main/java/com/example/overflow/MainActivity.java
@@ -5,20 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    final String url = "http://www.omdbapi.com/?i=tt3896198&apikey=89a740d6";
+    final String url = "http://35.247.146.48:3000/questions";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,34 +33,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        final TextView textView = findViewById(R.id.textview);
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(String response) {
-                Log.d("RESPON NIH", response);
+            public void onResponse(JSONArray response) {
+                Log.d("as", "onResponse: " + response.toString());
+                textView.setText(response.toString());
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("ERROR NIH", error.toString());
+                Log.e("error", "onErrorResponse: " + error.toString());
+
             }
-        });
-        stringRequest.setRetryPolicy(new RetryPolicy() {
-            @Override
-            public int getCurrentTimeout() {
-                return 50000;
-            }
+        })  {
 
             @Override
-            public int getCurrentRetryCount() {
-                return 50000;
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjNGVkMjBlNWI4YjY4MGI0NGIxMzM3OCIsImVtYWlsIjoic3Z4LmFubmV4aXZAZ21haWwuY29tIiwiaWF0IjoxNTQ4ODE0MTgzfQ.e9eDyyTwXjhOMtiL-F1a5_aU_wRJLxFiJ4BzRuQzxRQ");
+                return headers;
             }
+        };
 
-            @Override
-            public void retry(VolleyError error) throws VolleyError {
-
-            }
-        });
-
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
     }
 }
